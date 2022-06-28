547b5681d00dc1cc0fca4bf7937aa778c0238404
bzbzkang <65646883+bzbzkang@users.noreply.github.com>
2022-06-29 03:27:15 +0900

    finished question.Question

--- question.py
+++ question.py
@@ -1,11 +1,17 @@
 from typing import Tuple
+from typing import List
 
 
 class Question:
-    def __init__(self, question_list: Tuple[str, str]):
+    def __init__(self, question_tuple: List[Tuple[str, str]]):
         self.items = []
-        for token, content in question_list:
+        for token, content in question_tuple:
             if token in ('key', 'distractor'):
-                self.items.append([token, content[4:0]])
+                self.items.append([token, content[4:-1]])
             else:
-                self.items.append([token, content[2:0]])
+                self.items.append([token, content[2:-1]])
+
+
+if __name__ == "__main__":
+    question = Question([("stem", "# 강아지 다리 수는?;")])
+    print(question.items)
